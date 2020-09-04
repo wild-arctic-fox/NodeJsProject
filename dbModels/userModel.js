5160a936bce017d10ccf40c9f5e71369f14d11fb
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-09-04 16:19:49 +0300

    signIn/Up forms work

--- dbModels/userModel.js
+++ dbModels/userModel.js
@@ -8,6 +8,10 @@ const userModel = new Schema({
     type: String,
     required: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const UserModel = require("../dbModels/userModel");
+const { Exception } = require("handlebars");
 
 /////////////////////////////////////////////////////////
 // Router for displaying login or registration form
@@ -9,16 +10,8 @@ const router = Router();
 /////////////////////////////////////////////////////////
 // Display login and register forms
 router.get("/login", async (req, res) => {
-  const user = await UserModel.findById('5f4e8471b74ea91544f5831a');
-  req.session.user = user;
-  //req.session.isAuth = true;
-  req.session.save(err=>{
-    if(err){
-      throw new Error(err);
-    }
-    res.render("auth/login", {
-      title: "Sign in/up",
-    });
+  res.render("auth/login", {
+    title: "Sign in/up",
   });
 });
 
@@ -34,14 +27,46 @@ router.get("/logout", async (req, res) => {
 /////////////////////////////////////////////////////////
 // Recieve data to sign in
 router.post("/login/signIn", async (req, res) => {
-  req.session.isAuth = true; // already sing in
-  res.redirect("/")
+  try{
+    const {emailin:email, passwordin:password} = req.body;
+    const existedUser = await UserModel.findOne({email});
+    if(existedUser){
+      if(password === existedUser.password){
+        req.session.isAuth = true; // already sing in
+        req.session.user = existedUser;
+        req.session.save(err=>{
+          if(err){
+            throw new Error(err);
+          }
+          res.redirect("/")
+        });
+      }else{
+        res.redirect("/login#login");
+      }
+    }else{
+      res.redirect("/login#login");
+    }
+  }catch(e){
+      throw new Exception(e);
+  }
 });
 
 
 /////////////////////////////////////////////////////////
 // Recieve data to sign up
 router.post("/login/signUp", async (req, res) => {
-  res.redirect("/")
+  try{
+    const {username:name , email, password1:password, password2} = req.body;
+    const existedUser = await UserModel.findOne({email});
+    if(existedUser){
+      res.redirect("/login#register");
+    }else{
+      const user = new UserModel({name, email, password, cart:{items:[]}})
+      user.save();
+      res.redirect("/login#login");
+    }
+  } catch (e) {
+    throw new Exception(e)
+  }
 });
 module.exports = router;
