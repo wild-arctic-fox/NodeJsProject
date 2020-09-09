f540378aabec25788355bf17d3a7b7e1daaa228c
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-09-09 13:40:36 +0300

    Revert "image downloading refactored" :wq
    This reverts commit f09f47d8fff7dee4e67b8d54a00ad69b9ae1f681.

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
-const upload = require('../middleware/imgDownload')
+const multer = require("multer");
+const path = require("path");
 const auth = require("../middleware/auth");
 const UserModel = require("../dbModels/userModel");
 
@@ -7,6 +8,45 @@ const UserModel = require("../dbModels/userModel");
 // Router for displaying personal data
 /////////////////////////////////////////////////////////
 const router = Router();
+
+
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+// Set The Storage Engine
+/////////////////////////////////////////////////////////
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("myImage");//name if input type file
+
+// Check File Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
 
 /////////////////////////////////////////////////////////
